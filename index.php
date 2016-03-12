a54c62aad3df54596bfd29f63104eaec56021db3
Rachel Jaro <rachel@petixe.com>
2016-03-12 08:34:20 +0800

    add zopim

--- index.php
+++ index.php
@@ -283,5 +283,15 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/slider.js"></script>
     <script src="js/bootstrap.js"></script>
+
+    <!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3DTVhI6ueFMi2LHvDIs3ziMRyqEmuJvh";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zopim Live Chat Script-->
 </body>
 </html>
